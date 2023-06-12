2c048d237a75b2dc1ef696c573e1c930004e0e59
Fuksik64 <fuksik644@gmail.com>
2023-06-12 20:21:15 +0200

    done

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,3 @@
 <?php
 
-use App\Http\Controllers\BranchController;
-use App\Http\Controllers\CatController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Resources\BranchResource;
 use Illuminate\Support\Facades\Route;
-
-
-Route::resource('branch', BranchController::class)->only('show', 'index');
-Route::resource('employee', EmployeeController::class)->only('show', 'index');
-Route::resource('cat', CatController::class)->only('show', 'index');
